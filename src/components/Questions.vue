ceed03f6ae099e2f80ed5f54c1c4ca551df88502
Bruno Marini <bruno.marini1@gmail.com>
2019-05-16 09:15:20 -0300

    Questions [+] {list,create,delete}

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,72 +1,81 @@
-<template>
-	<div>
-		<vs-row vs-justify="center">
-		  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-					<vs-row vs-justify="center">
-						<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
-							<vs-button class="px-5 create" color="success" icon="add" @click="func='';popupActivo=true">
-								<h3>Nova Questão</h3>
-							</vs-button>
-						</vs-col>
-					</vs-row>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-		<vs-row vs-justify="center">
-		  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-		      <div slot="header">
-		        <h2 icon="search">
-		          Pesquisar
-		        </h2>
-		      </div>
-		      <div class="centerx default-input">
-		        <vs-input class="inputx" placeholder="Digite alguma busca..." v-model="searchText"/>
-		      </div>
-		      <div slot="footer">
-		        <vs-row vs-justify="flex-end">
-		          <vs-button class="px-5" color="primary" icon="search"></vs-button>
-		        </vs-row>
-		      </div>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-		<vs-row>
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="3">
-			</vs-col>
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-				<vs-divider />
-			</vs-col>
-		</vs-row>
-		{{questions[0].question.id}}
-		{{questions[1].question.id}}
-		{{questions[2].question.id}}
-		{{questions[3].question.id}}
-		<!-- {{questions}} -->
-		<vs-row vs-justify="center" v-for="(question, index) in questions" :key="`question-${question.question.id_question}`" class="question-card">
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-		      <div slot="header">
-		        <h2 icon="search">
-		          {{ question.question.text_question }}
-		        </h2>
-		      </div>
-					{{ question.question.description_question }}
-					<div v-for="(choice,index) in question.choices" :key="`choice-${choice.id_choice}`">
-						<div><span :class="choice.trueness_choice === 'v' ? 'question-answer' : ''">{{letras[index]}}</span> {{ choice.description_choice }}</div>
+<template lang="html">
+	<div class="questions">
+		<header class="page-header px-5 d-flex justify-content-end align-items-center">
+			<h1 class="page-title text-center">Questões</h1>
+			<button class="__action __action-outline add btn" type="button">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+					<path d="M0 0h24v24H0z" fill="none"/>
+				</svg>
+				Adicionar
+			</button>
+		</header>
+		<article class="container">
+			<createQuestion class="mb-3" @question-saved='refresh'></createQuestion>
+			<form class="__card p-3">
+			  <div class="form-row">
+			    <div class="col-10">
+			      <input type="text" class="__rounded form-control h-100" placeholder="Digite algum texto...">
+			    </div>
+			    <div class="col-2 d-flex align-items-end">
+						<button class="__action search btn" type="button">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+								<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+								<path d="M0 0h24v24H0z" fill="none"/>
+							</svg>
+							Pesquisar
+						</button>
+			    </div>
+			  </div>
+			</form>
+			<div class="wrapper mt-4">
+				<div class="question __card mt-3" v-for="(question, index) in questions" :key="question.id_question">
+					<div class="question-header d-flex justify-content-between align-items-center">
+						<div class="d-flex">
+							<div class="action link __rounded-tl">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path d="M0 0h24v24H0z" fill="none"/>
+									<path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/>
+								</svg>
+							</div>
+							<div class="action copy">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path fill="none" d="M0 0h24v24H0z"/>
+									<path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"/>
+								</svg>
+							</div>
+							<div class="action edit __rounded-br" @click="editQuestion(index)">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+									<path d="M0 0h24v24H0z" fill="none"/>
+								</svg>
+							</div>
+						</div>
+						<div class="action delete __rounded-tr" @click="deleteQuestion(question.id_question)">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+								<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/>
+								<path fill="none" d="M0 0h24v24H0V0z"/>
+							</svg>
+						</div>
 					</div>
-		      <div slot="footer">
-		        <vs-row vs-justify="space-between">
-		          <vs-button color="dark" icon="delete" @click="handleDelete(question.id_question)"></vs-button>
-							<vs-button color="warning" icon="edit" @click="func='edit';popupActivo=true"></vs-button>
-		        </vs-row>
-		      </div>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-
-		<createQuestion :popupActivo='popupActivo' :func="func"></createQuestion>
+					<div class="question-body p-4">
+						<div class="question-text">
+							{{question.text_question}}
+							<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat consequat mauris nunc. Massa tempor nec feugiat nisl pretium fusce. Accumsan sit amet nulla facilisi. Magna etiam tempor orci eu lobortis elementum. -->
+						</div>
+						<hr class="question-divider">
+						<div class="question-choices">
+							<div class="row justify-content-center">
+								<div class="col-sm-12 col-md-6 col-lg-2 text-center" v-for="(choice, index) in question.choices" :key="choice.id_choice">
+									<span :class="choice.trueness_choice == 'v' ? 'active' : ''">{{letters[index]}})</span>
+									{{choice.description_choice}}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
 	</div>
 </template>
 <script>
@@ -75,68 +84,101 @@ import createQuestion from './Questions/create.vue';
 
 export default {
   name: "Questions",
-  components: { createQuestion },
+	components: { createQuestion },
   data() {
     return {
-			searchText: "",
-			popupActivo: false,
 			questions: [],
-			letras: ['a)','b)','c)','d)','e)']
+			letters: ['A', 'B', 'C', 'D', 'E']
 		}
   },
 	created: function() {
 		axios.defaults.baseURL = 'http://localhost:3000';
-		this.reload();
+		this.refresh();
 	},
 	methods: {
-		getListedQuestions() {
-			return this.questions;
-		},
-    reload() {
+		async refresh () {
+			// Reset
 			this.questions = [];
-      axios.get('/api/question').then((response) => {
-				let questions = response.data;
+
+			// New
+			let questions;
+      await axios.get('/api/question').then(async response => {
+				questions = response.data;
 
 				for (let question of questions) {
-					axios.get('/api/choice/q/'+question.id_question).then((response) => {
-						this.questions.push({
-							question: question,
-							choices: response.data
-						})
-					}).then(_ => {
-						console.log(this.questions);
-						setTimeout(_ => {
-							this.$forceUpdate()
-							this.fetchData()
-						}, 100);
-					})
-					.catch((error) => {
-						console.log(error)
+					await axios.get('/api/choice', {params: {where: {id_question: question.id_question}}}).then(response => {
+						question.choices = response.data;
 					})
 				}
-
       }).catch((error) => {
         console.log(error)
       })
-    },
-		handleDelete(questionToDelete) {
-			console.log(questionToDelete)
-			axios.delete('/api/question/'+questionToDelete).then(_ => {
-				this.reload()
+
+			this.questions = questions.sort((a,b) => (a.id_question < b.id_question) ? 1 : ((b.id_question < a.id_question) ? -1 : 0));
+			console.log("Questions: ", this.questions)
+		},
+		deleteQuestion (id_question) {
+			axios.delete('/api/question/'+id_question).then(_ => {
+				this.refresh()
 			})
+		},
+		editQuestion (index) {
+			this.copyToForm(index);
+		},
+		copyToForm (index) {
+			console.log(this.questions[index]);
+			
 		}
 	}
 }
 </script>
 <style scoped>
-.inputx {
-	padding: 10px 10px 0 10px;
-	width: 100%
+.question {
+	color: #4C5454;
 }
-.create {
-	width: 100% !important
+.question-header {
+
 }
-.question-answer {
-  font-weight: bold;
+/* Disable "flex: 1 1" wich bootstrap do */
+.question-header>* {
+	flex: inherit !important
+}
+.action {
+	height: 37px;
+	width: 37px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	cursor: pointer;
+}
+
+.action svg:not(.delete) {
+	fill: white
+}
+
+.action.link {
+	background-color: #1EA896;
+}
+.action.copy {
+	background-color: #4C5454;
+}
+.action.edit {
+	background-color: #FF715B;
+}
+.action.delete svg {
+	fill: #4C5454;
+}
+
+.question-text {
+	text-align: justify;
+	font-size: 1.1rem;
+	font-weight: 400;
+}
+.question-divider {
+	border-top: 1px dashed #4C5454;
+}
+.question-choices span.active{
+	font-weight: 700
 }
 </style>
--- src/components/Questions/create.vue
+++ src/components/Questions/create.vue
@@ -1,34 +1,58 @@
 <template lang="html">
-  <vs-popup classContent="popup-example" color="success" :title="func == 'edit'? 'Editar' : 'Cadastrar' | Questão" :active.sync="popupActivo">
-		<div class="wrapper">
-
-			<vs-row vs-justify="center">
-				<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
-					<vs-input size="large" class="inputx question-title" placeholder="Titulo da questão" v-model="questionTitle"/>
-					<v-textarea placeholder="Enunciado da questão" v-model="questionDesc" auto-grow rows="1"></v-textarea>
-					<div class="choices">
-						<template v-for="(option, index) in choices">
-							<vs-row vs-justify="center">
-								<v-flex :key="index">
-									<v-checkbox v-model="correctAnswer" :value="option.value"></v-checkbox>
-								</v-flex>
-								<v-flex lg11 sm11 :key="index + option">
-									<v-textarea v-model="option.body" :label="option.label" auto-grow rows="1"></v-textarea>
-								</v-flex>
-							</vs-row>
-						</template>
+	<div class="question-create __rounded p-3">
+		<form class="__card p-3 mb-3">
+			<div class="form-group">
+				<label for="textarea">Enunciado</label>
+				<b-form-textarea
+				id="textarea"
+				v-model="text"
+				placeholder="Digite algo..."
+				rows="3"
+				max-rows="6"
+				></b-form-textarea>
+			</div>
+		</form>
+		<form class="__card p-3">
+			<label for="choices">Alternativas</label>
+			<b-form-radio-group id="choices" v-model="selected" name="letter" buttons button-variant="outline-success">
+				<div class="form-row mb-2" v-for="index in 5" :key="index">
+					<div class="col-1 d-flex justify-content-center">
+						<b-form-radio :value="index-1" class="h-100"></b-form-radio>
 					</div>
-				</vs-col>
-			</vs-row>
-			<vs-row vs-justify="center">
-				<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
-					<vs-button class="create" color="success" icon="add" @click="handleSave">
-						<div></div>
-					</vs-button>
-				</vs-col>
-			</vs-row>
+					<div class="col-11">
+						<input v-model="choices[index-1].text" type="text" class="form-control h-100" :placeholder="`${index}ª alternativa`">
+					</div>
+				</div>
+			</b-form-radio-group>
+			<div v-if="selected !== ''" class="trueness-choice p-2 __rounded d-inline-flex"><b class="mr-2">Verdadeira: </b>{{ selected+1 }}ª alternativa</div>
+		</form>
+		<div class="question-create-footer mt-3">
+			<div class="row">
+				<div class="col-sm-12 col-md-7 d-flex justify-content-center align-items-center">
+					<div class="bad row">
+						<div class="col-sm-12 col-md-2 pr-0">
+							<p class="action text-center mb-0 py-2 __rounded">
+								Cancelar
+							</p>
+						</div>
+						<div class="col-sm-12 col-md-2 pl-0">
+							<p class="action text-center mb-0 py-2 __rounded">
+								Limpar
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-5">
+					<button class="__action add btn px-5 py-2 w-100" type="button" @click="save">
+						Adicionar
+					</button>
+				</div>
+			</div>
 		</div>
-  </vs-popup>
+		<!-- {{selected}}
+		{{text}}
+		{{choices}} -->
+	</div>
 </template>
 
 <script>
@@ -36,22 +60,16 @@ import axios from 'axios';
 
 export default {
 	name: "createQuestion",
-	props: {
-		popupActivo: Boolean,
-		func: String,
-		title: String
-	},
   data(){
     return {
-			questionTitle: '',
-			questionDesc: '',
-			answer: '',
+			selected: '',
+			text: '',
 			choices: [
-				{ value: '1', label: 'a)', body: '' },
-				{ value: '2', label: 'b)', body: '' },
-				{ value: '3', label: 'c)', body: '' },
-				{ value: '4', label: 'd)', body: '' },
-				{ value: '5', label: 'e)', body: '' }
+				{ text: '' },
+				{ text: '' },
+				{ text: '' },
+				{ text: '' },
+				{ text: '' }
 			]
     }
   },
@@ -67,40 +85,30 @@ export default {
 		}
 	},
 	methods: {
-    handleClose() {
-      this.popupActivo = false;
-			this.fetchData()
-    },
-    handleSave() {
-			let question = {
-	      text_question: this.questionTitle,
-	      description_question: this.questionDesc
-			}
-			console.log(question)
-      axios.post('/api/question', question).then(response => {
+		async save () {
+			let status = await axios.post('/api/question', { text_question: this.text }).then(response => {
 				let choices = [];
 
 				for (var i = 0; i < this.choices.length; i++) {
 					let choice = {};
 
 					choice.id_question = response.data.id_question;
-					choice.description_choice = this.choices[i].body;
-					choice.trueness_choice = this.choices[i].value == this.answer ? 'v' : 'f';
+					choice.description_choice = this.choices[i].text;
+					choice.trueness_choice = this.selected == i ? 'v' : 'f';
 
 					choices.push(choice);
 				}
 
 				console.log("Escolhas: ", choices)
-				axios.post('/api/choice', choices).then(_ => {
-					this.handleClose()
-				});
-			});
 
+				axios.post('/api/choice', choices);
 
-    },
-    getListedQuestions() {
-      return (this.searchStr) ? this.listedQuestions : this.questions;
-    },
+				return true;
+			}).catch(error => { alert(error); return false });
+
+			if (status === true)
+				this.$emit('question-saved')
+		},
     handleEdit(questionToEdit) {
       this.answer = [questionToEdit.answer]
       this.text = questionToEdit.text
@@ -118,25 +126,50 @@ export default {
 				this.reload()
 			})
     }
+	},
+	mounted() {
+		// Remove "btn-group" and "btn-group-toggle" classes that "b-form-radio-group" component adds when has the prop "button"
+		let choices = document.getElementById('choices');
+		choices.classList.remove('btn-group');
+		choices.classList.remove('btn-group-toggle');
+
+		// Hide the default input[type="radio"]
+		let radio = document.querySelectorAll('#choices input[type="radio"]');
+		radio.forEach(element => element.style.opacity = '0')
+		// radio.classList.add('hide');
 	}
 }
 </script>
 
 <style lang="css" scoped>
-.popup-example .con-select {
-  margin-left: 10px;
-  width: 50%;
-  margin-bottom: 10px;
+.question-create {
+	border: 2px dashed #4C5454;
 }
-
-.inputx {
-	width: 100% !important;
-	margin-top: 5px;
+label {
+	font-size: 1.1rem;
+	font-weight: 500;
+	color: #4C5454
 }
-.wrapper {
-	padding: 0px 10px
+.trueness-choice {
+	color: white;
+	background-color: #4C5454
 }
-.create {
-	width: 100%
+.action {
+	font-weight: bold
+}
+.__action.add {
+	background-color: #1EA896;
+	color: white;
+	font-weight: 700 !important
+}
+.__action.add svg {
+	fill: #fff;
+}
+.bad .action {
+	transition: .3s
+}
+.bad .action:hover {
+	background-color: #4C5454;
+	color: white
 }
 </style>
