f63568ec7ae750c28c4bdaf2bfc9bf3873db2ee1
Bruno Marini <bruno.marini1@gmail.com>
2019-05-16 11:02:12 -0300

    Exams [+] {list, create, delete}

--- src/components/Exams.vue
+++ src/components/Exams.vue
@@ -1,112 +1,163 @@
-<template>
-	<div>
-		<vs-row vs-justify="center">
-		  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-					<vs-row vs-justify="center">
-						<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
-							<vs-button class="px-5 create" color="success" icon="add" @click="popupActivo=true">
-								<h3>Nova Avaliação</h3>
-							</vs-button>
-						</vs-col>
-					</vs-row>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-		<vs-row vs-justify="center">
-		  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-		      <div slot="header">
-		        <h2 icon="search">
-		          Pesquisar
-		        </h2>
-		      </div>
-		      <div class="centerx default-input">
-		        <vs-input class="inputx" placeholder="Digite alguma busca..." v-model="searchText"/>
-		      </div>
-		      <div slot="footer">
-		        <vs-row vs-justify="flex-end">
-		          <vs-button class="px-5" color="primary" icon="search"></vs-button>
-		        </vs-row>
-		      </div>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-		<vs-row>
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="3">
-			</vs-col>
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-				<vs-divider />
-			</vs-col>
-		</vs-row>
-		{{questions[0].id}}
-		<vs-row vs-justify="center" v-for="(question, index) in getListedQuestions()" :key="question.id_question" class="question-card">
-			<vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-		    <vs-card>
-		      <div slot="header">
-		        <h2 icon="search">
-		          {{ question.text_question }}
-		        </h2>
-		      </div>
-					{{ question.description_question }}
-		      <div slot="footer">
-		        <vs-row vs-justify="space-between">
-		          <vs-button color="dark" icon="delete" @click="handleDelete(question.id_question)"></vs-button>
-							<vs-button color="warning" icon="edit"></vs-button>
-		        </vs-row>
-		      </div>
-		    </vs-card>
-		  </vs-col>
-		</vs-row>
-
-		<createQuestion :popupActivo='popupActivo'></createQuestion>
+<template lang="html">
+	<div class="exams">
+		<header class="page-header px-5 d-flex justify-content-end align-items-center">
+			<h1 class="page-title text-center">Avaliações</h1>
+			<button class="__action __action-outline add btn" type="button">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+					<path d="M0 0h24v24H0z" fill="none"/>
+				</svg>
+				Adicionar
+			</button>
+		</header>
+		<article class="container">
+			<createExam class="mb-3" @saved='refresh'></createExam>
+			<form class="__card p-3">
+			  <div class="form-row">
+			    <div class="col-10">
+			      <input type="text" class="__rounded form-control h-100" placeholder="Digite algum texto...">
+			    </div>
+			    <div class="col-2 d-flex align-items-end">
+						<button class="__action search btn" type="button">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+								<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+								<path d="M0 0h24v24H0z" fill="none"/>
+							</svg>
+							Pesquisar
+						</button>
+			    </div>
+			  </div>
+			</form>
+			<div class="wrapper mt-4">
+				<div class="exam __card mt-3" v-for="(exam, index) in exams" :key="exam.id_test">
+					<div class="exam-header d-flex justify-content-between align-items-center">
+						<div class="d-flex">
+							<!-- <div class="action link __rounded-tl">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path d="M0 0h24v24H0z" fill="none"/>
+									<path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/>
+								</svg>
+							</div> -->
+							<div class="action copy __rounded-tl">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path fill="none" d="M0 0h24v24H0z"/>
+									<path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"/>
+								</svg>
+							</div>
+							<div class="action edit __rounded-br" @click="editExam(index)">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+									<path d="M0 0h24v24H0z" fill="none"/>
+								</svg>
+							</div>
+						</div>
+						<div class="action delete __rounded-tr" @click="deleteExam(exam.id_test)">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+								<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/>
+								<path fill="none" d="M0 0h24v24H0V0z"/>
+							</svg>
+						</div>
+					</div>
+					<div class="exam-body p-4">
+						<h3 class="exam-name">{{exam.name_test}}</h3>
+						<hr class="exam-divider">
+						<div class="exam-text">
+							{{exam.description_test}}
+							<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat consequat mauris nunc. Massa tempor nec feugiat nisl pretium fusce. Accumsan sit amet nulla facilisi. Magna etiam tempor orci eu lobortis elementum. -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
 	</div>
 </template>
 <script>
 import axios from 'axios';
-import createQuestion from './Questions/create.vue';
+import createExam from './Exams/create.vue';
 
 export default {
   name: "Exams",
-  components: { createQuestion },
+	components: { createExam },
   data() {
     return {
-			searchText: "",
-			popupActivo: false,
-			questions: []
+			exams: []
 		}
   },
 	created: function() {
 		axios.defaults.baseURL = 'http://localhost:3000';
-		this.reload();
+		this.refresh();
 	},
 	methods: {
-		getListedQuestions() {
-			return this.questions;
-		},
-    reload() {
-      axios.get('/api/question').then((response) => {
-				this.questions = response.data;
-				console.log(this.questions)
+		async refresh () {
+      await axios.get('/api/test').then(async response => {
+				this.exams = response.data.sort((a,b) => (a.id_test < b.id_test) ? 1 : ((b.id_test < a.id_test) ? -1 : 0));;
       }).catch((error) => {
         console.log(error)
       })
-    },
-		handleDelete(questionToDelete) {
-			console.log(questionToDelete)
-			axios.delete('/api/question/'+questionToDelete).then(_ => {
-				this.reload()
+
+			console.log("Exams: ", this.exams)
+		},
+		deleteExam (id_exam) {
+			axios.delete('/api/test/'+id_exam).then(_ => {
+				this.refresh()
 			})
+		},
+		editExam (index) {
+			this.copyToForm(index);
+		},
+		copyToForm (index) {
+			console.log(this.exams[index]);
 		}
 	}
 }
 </script>
 <style scoped>
-.inputx {
-	padding: 10px 10px 0 10px;
-	width: 100%
+.exam {
+	color: #4C5454;
 }
-.create {
-	width: 100% !important
+.exam-header {
+
+}
+/* Disable "flex: 1 1" wich bootstrap do */
+.exam-header>* {
+	flex: inherit !important
+}
+.action {
+	height: 37px;
+	width: 37px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	cursor: pointer;
+}
+
+.action svg:not(.delete) {
+	fill: white
+}
+
+.action.link {
+	background-color: #1EA896;
+}
+.action.copy {
+	background-color: #4C5454;
+}
+.action.edit {
+	background-color: #FF715B;
+}
+.action.delete svg {
+	fill: #4C5454;
+}
+
+.exam-text {
+	text-align: justify;
+	font-size: 1.1rem;
+	font-weight: 400;
+}
+.exam-divider {
+	border-top: 1px dashed #4C5454;
+}
+.exam-choices span.active{
+	font-weight: 700
 }
 </style>
---
+++ src/components/Exams/create.vue
@@ -0,0 +1,113 @@
+<template lang="html">
+	<div class="question-create __rounded p-3">
+		<form class="__card p-3 mb-3">
+			<div class="form-group">
+				<label for="textarea">Nome</label>
+				<input v-model="name" type="text" class="__rounded form-control form-control-lg h-100" placeholder="Nome da avaliação">
+			</div>
+			<div class="form-group">
+				<label for="textarea">Orientações</label>
+				<b-form-textarea
+				id="textarea"
+				v-model="text"
+				placeholder="Orientações da avaliação"
+				size="lg"
+				rows="3"
+				max-rows="6"
+				></b-form-textarea>
+			</div>
+		</form>
+		<div class="question-create-footer mt-3">
+			<div class="row">
+				<div class="col-sm-12 col-md-7 d-flex justify-content-center align-items-center">
+					<div class="bad row">
+						<div class="col-sm-12 col-md-2 pr-0">
+							<p class="action text-center mb-0 py-2 __rounded">
+								Cancelar
+							</p>
+						</div>
+						<div class="col-sm-12 col-md-2 pl-0">
+							<p class="action text-center mb-0 py-2 __rounded">
+								Limpar
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-5">
+					<button class="__action add btn px-5 py-2 w-100" type="button" @click="save">
+						Adicionar
+					</button>
+				</div>
+			</div>
+		</div>
+		<!-- {{selected}}
+		{{text}}
+		{{choices}} -->
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+	name: "createExam",
+  data(){
+    return {
+			name: '',
+			text: ''
+    }
+  },
+	methods: {
+		async save () {
+			let status = await axios.post('/api/test', {name_test: this.name, description_test: this.text }).then(response => {
+				console.log(response)
+
+				return true;
+			}).catch(error => { alert(error); return false });
+
+			if (status === true)
+				this.$emit('saved')
+		},
+    handleDelete(questionToDelete) {
+			console.log(questionToDelete)
+      axios.delete('/api/question/'+questionToDelete.id_questao).then(_ => {
+				this.reload()
+			})
+    }
+	}
+}
+</script>
+
+<style lang="css" scoped>
+.question-create {
+	border: 2px dashed #4C5454;
+}
+label {
+	font-size: 1.1rem;
+	font-weight: 500;
+	color: #4C5454
+}
+.trueness-choice {
+	color: white;
+	background-color: #4C5454
+}
+.action {
+	font-weight: bold
+}
+.__action.add {
+	background-color: #1EA896;
+	color: white;
+	font-size: 1.2rem;
+	font-weight: 700 !important
+}
+.__action.add svg {
+	fill: #fff;
+}
+.bad .action {
+	transition: .3s
+}
+.bad .action:hover {
+	background-color: #4C5454;
+	color: white
+}
+</style>
