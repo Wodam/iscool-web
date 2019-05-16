a5b5f173a5099ca7a87b856b6e42f9633e9fddbc
Bruno Marini <bruno.marini1@gmail.com>
2019-05-16 02:00:56 -0300

    Roboto font [^] | Classes [+] {rounded, page-header} | Style resets [+] {links}

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="d-flex">
 		<SideBar></SideBar>
-    <router-view class="container"></router-view>
+    <router-view class="main"></router-view>
   </div>
 </template>
 
@@ -21,14 +21,88 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700');
+/* --- FONTS --- */
+/* TODO: change to local files */
+@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900');
 
-#app {
-  background-color: whitesmoke;
-	font-family: 'Roboto Slab', serif;
+/* --- GLOBALS --- */
+/* ~ Rounded ~ */
+.__rounded-100 {
+  border-radius: 100% !important
+}
+.__rounded {
+  border-radius: 5px !important
+}
+.__rounded-tl {
+  border-top-left-radius: 5px !important
+}
+.__rounded-tr {
+  border-top-right-radius: 5px !important
+}
+.__rounded-bl {
+  border-bottom-left-radius: 5px !important
+}
+.__rounded-br {
+  border-bottom-right-radius: 5px !important
 }
 
-.container {
-	margin-left: 250px !important;
+/* --- RESETS --- */
+a:not([aria-disabled="true"]) {
+	color: inherit !important;
+}
+
+/* --- OTHERS --- */
+#app {
+  background-color: whitesmoke;
+	font-family: 'Roboto', sans-serif;
+}
+
+.main {
+	/* margin-left: 250px !important; */
+}
+
+.btn {
+	border-radius: 5px !important
+}
+
+/* Page Header */
+.page-header {
+	height: 15vh;
+	width: 100%;
+	background-color: white;
+	position: relative;
+}
+
+.page-title {
+	font-weight: 500;
+	color: #4C5454;
+	font-size: 3rem
+}
+
+/* Add button */
+.page-header .add {
+	position: absolute;
+	color: #1EA896;
+	border: 2px solid #1EA896;
+	font-weight: 500;
+	font-size: 1.1rem;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.page-header .add svg {
+	fill: #1EA896;
+	margin-right: 6px
+}
+
+.page-header .add:hover {
+	background-color: #1EA896;
+	color: white;
+}
+
+.page-header .add:hover svg{
+	fill: white;
 }
 </style>
